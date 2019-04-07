0f963ab3214ca7f91e98f6bf63be84e979230dac
prakash sharma <expertinjava@gmail.com>
2019-04-07 11:48:01 +0530

    First commit

---
+++ SAMPLE/WebContent/login.jsp
@@ -0,0 +1,29 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>	
+<!DOCTYPE html>
+<html>
+<body onload='document.loginForm.username.focus();'>
+    <h1>Spring Security 5 - Login Form</h1>
+ 
+    <c:if test="${not empty errorMessge}"><div style="color:red; font-weight: bold; margin: 30px 0px;">${errorMessge}</div></c:if>
+ 
+    <form:form id="regForm" modelAttribute="user" action="registerProcess" method="post">
+        <table>
+            <tr>
+                <td>UserName:</td>
+                <td><input type='text' name='username' value=''></td>
+            </tr>
+            <tr>
+                <td>Password:</td>
+                <td><input type='password' name='password' /></td>
+            </tr>
+            <tr>
+                <td colspan='2'><input name="submit" type="submit" value="submit" /></td>
+            </tr>
+        </table>
+        <input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}" />
+    </form:form>
+</body>
+</html>
---
+++ SAMPLE/src/main/java/controller/HelloController.java
@@ -0,0 +1,45 @@
+package controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView; 
+@Controller
+public class HelloController {
+	String message = "Welcome to Spring MVC!";
+ 
+	@RequestMapping("/hello")
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("in controller");
+ 
+		//return "index";
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+	
+	@RequestMapping(value = "/registerProcess", method = RequestMethod.POST)
+	  public ModelAndView addUser(HttpServletRequest request, HttpServletResponse response,
+	  @ModelAttribute("user") User user) {
+		
+		System.out.println("Inside register process");
+	  //userService.register(user);
+	  return new ModelAndView("welcome", "username", user.getUsername());
+	  }
+	
+	/*@RequestMapping(value="/logout", method=RequestMethod.GET)  
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {  
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();  
+        if (auth != null){      
+           new SecurityContextLogoutHandler().logout(request, response, auth);  
+        }  
+         return "redirect:/";  
+     }  */
+}
