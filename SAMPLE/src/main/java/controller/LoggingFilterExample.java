b489a21775aefdc985b15a1d6d935f3c9290677b
prakash sharma <expertinjava@gmail.com>
2019-04-07 12:42:59 +0530

    Added loggers

--- SAMPLE/src/main/java/controller/LoggingFilterExample.java
+++ SAMPLE/src/main/java/controller/LoggingFilterExample.java
@@ -1,11 +1,20 @@
 package controller;
 
-import java.io.*;
+import java.io.IOException;
+import org.apache.log4j.Logger;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 public final class LoggingFilterExample implements Filter {
+	
+	private final Logger logger = Logger.getLogger(LoggingFilterExample.class);
+	
 	private FilterConfig filterConfigObj = null;
 
 	public void init(FilterConfig filterConfigObj) {
@@ -26,6 +35,7 @@ ServletResponse response,
   	filterConfigObj.getServletContext().log("**************************");
   	filterConfigObj.getServletContext().log("User Logged !");
   	filterConfigObj.getServletContext().log("username is "+request.getParameter("username"));
+  	logger.info("Filter executed with-->"+request.getParameter("username"));
   }
 
 	public void destroy() {
